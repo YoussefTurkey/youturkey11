4b26f2f918cc5bc87b514806555670427e49f289
YoussefTurkey <youssef.turkey11@gmail.com>
2025-09-02 22:02:46 +0300

    fixing deployment

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,16 +1,30 @@
+"use client";
+// Providers
+import ClientThemeProvider from "@/app/themes/ClientThemeProvider";
+import ClientLanguageProvider from "@/app/lang/ClientLanguageProvider";
+// Components
+import Header from "@/app/components/layout/Header";
+import Footer from "@/app/components/layout/Footer";
+
 export default function LangLayout({
   children,
   params,
 }: {
   children: React.ReactNode;
-  params: { lang: string };
+  params: { lang: "en" | "ar" };
 }) {
   const direction = params.lang === "ar" ? "rtl" : "ltr";
 
   return (
     <html lang={params.lang} dir={direction}>
       <body>
-        {children}
+        <ClientThemeProvider>
+          <ClientLanguageProvider defaultLanguage={params.lang}>
+            <Header />
+            {children}
+            <Footer />
+          </ClientLanguageProvider>
+        </ClientThemeProvider>
       </body>
     </html>
   );
