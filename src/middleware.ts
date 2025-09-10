f5af419a1fb75ac4f5dc519c76e81e3c0186de14
YoussefTurkey <youssef.turkey11@gmail.com>
2025-09-11 00:52:12 +0300

    fixing middleware.ts 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,20 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
+
   const pathnameHasLocale = /^\/[a-z]{2}/.test(pathname);
-  
+
   if (!pathnameHasLocale) {
-    // إعادة توجيه إلى النسخة الإنجليزية افتراضيًا
-    return NextResponse.redirect(new URL(`${pathname}`, request.url));
+    // 👇 لو عاوز دايمًا يحول للإنجليزي
+    return NextResponse.redirect(new URL(`/en${pathname}`, request.url));
   }
+
+  // 👇 لو عاوز يسيب الـ routes زي ما هي من غير أي تحويل
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
