00f94e84ae9f77412b846be27ea644272d487981
YoussefTurkey <youssef.turkey11@gmail.com>
2025-09-12 20:10:47 +0300

    scroll btn

--- src/app/components/ui/ScrollUp.tsx
+++ src/app/components/ui/ScrollUp.tsx
@@ -1,12 +1,16 @@
-'use client';
-import { useEffect, useState } from 'react';
+"use client";
+// Importing React Hooks
+import { useEffect, useState } from "react";
+// Importing React-Icons
 import { TiArrowUpThick } from "react-icons/ti";
 // using Translation
 import { useLanguage } from "@/app/lang/LanguageProvider";
+// Improting Next components
+import { usePathname } from "next/navigation";
 
 const ScrollUp = () => {
   const { language } = useLanguage();
-
+  const pathname = usePathname();
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
@@ -14,23 +18,40 @@ const ScrollUp = () => {
       setIsVisible(window.scrollY > 200);
     };
 
-    window.addEventListener('scroll', toggleVisibility);
-    return () => window.removeEventListener('scroll', toggleVisibility);
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
   }, []);
 
   const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
-    <button
-      onClick={scrollToTop}
-      className={`hidden sm:block fixed bottom-15 md:bottom-6 z-1000 ${language === 'en' ? 'right-4 sm:right-10' : 'left-7 sm:left-10'} bg-[hsl(var(--secondary))] text-[hsl(var(--background))] p-3 rounded-full shadow-lg transition-opacity duration-300 cursor-pointer ${
-        isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'
-      }`}
-    >
-      <TiArrowUpThick size={15} />
-    </button>
+    <>
+      {pathname === "/" ? (
+        <button
+          onClick={scrollToTop}
+          className={`hidden sm:block fixed bottom-15 md:bottom-6 z-1000 ${
+            language === "en" ? "right-4 sm:right-10" : "left-7 sm:left-10"
+          } bg-[hsl(var(--secondary))] text-[hsl(var(--background))] p-3 rounded-full shadow-lg transition-opacity duration-300 cursor-pointer ${
+            isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+          }`}
+        >
+          <TiArrowUpThick size={15} />
+        </button>
+      ) : 
+        <button
+          onClick={scrollToTop}
+          className={`block fixed bottom-15 md:bottom-6 z-1000 ${
+            language === "en" ? "right-4 sm:right-10" : "left-7 sm:left-10"
+          } bg-[hsl(var(--secondary))] text-[hsl(var(--background))] p-3 rounded-full shadow-lg transition-opacity duration-300 cursor-pointer ${
+            isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+          }`}
+        >
+          <TiArrowUpThick size={15} />
+        </button>
+      }
+    </>
   );
 };
 
